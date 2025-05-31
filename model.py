74df0a9c6dcfb63c9295bc4b03f887db31979138
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-05-31 10:34:44 +0530

    Encoder Completed

--- model.py
+++ model.py
@@ -138,6 +138,16 @@ class EncoderBlock(nn.Module):
         x = self.residual_connections[1](self.feed_forward_block)
         return x
         
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, mask):
+        for layer in self.layers: # Apply one laye after another
+            x = layer(x, mask)
+        return self.norm(x) # Apply the normlaization
 
 
 
