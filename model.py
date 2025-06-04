8cb40cb92b65e86f65224a0bbe613b6a6d7bac81
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-06-04 11:52:22 +0530

    Transformer Class

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch # A Python library for building neural networks
 import  torch.nn as nn #  To build layers for neural networks
 import math # To perform math functions
 
-class ImputEmbeddings(nn.Module):
+class InputEmbeddings(nn.Module):
     def __init__(self, d_model: int, vocab_size: int):
         super().__init__() # Initialize parent class (nn.Module)
         self.d_model = d_model
@@ -182,3 +182,27 @@ class ProjectionLayer(nn.Module):
     def forward(self, x):
         # (Batch, seq_len, vocab_size) --> (Batch, seq_len, vocab_seze)
         return torch.log_softmax(self.proj(x), dim=-1)
+    
+class Transformer(nn.Module):
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbeddings, tgt_embed: InputEmbeddings, src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer: ProjectionLayer) -> None:
+        super().__init()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
+
+    def encode(self, src, src_mask):
+        src = self.src_embed(src) # Apply the embedding
+        src = self.src_pos(src) # Apply the positional encoding
+        return self.encoder(src, src_mask) # Finally we apply the encoder
+
+    def decode(self, encoder_output, src_mask, tgt, tgt_mask):
+        tgt = self.tgt_embed(tgt) # Apply ther target embedding to the target sentence
+        tgt = self.tgt_pos(tgt) # Apply the positional encoding to the target sentence
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask) # This is basically the forward method of the lass Decoder
+    
+    def project(self, x):
+        self.projection_layer(x)
