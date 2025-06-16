f40672a93ef0a928b8398113f8b1c0c98e2ad581
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-06-16 13:35:42 +0530

    training loop

--- train.py
+++ train.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, Dataloader, random_split
 
+from dataset import BilingualDataset, causal_mask
+
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
@@ -38,4 +40,24 @@ def get_ds(config):
     train_ds_size = int(0.9 * len(ds_raw))
     val_ds_size = int(ds_raw) - train_ds_size
     train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
-    
+
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids)) # Maximum length of source language
+        max_len_tgt = max(max_len_tgt, len(tgt_ids)) # Maximum length of the target langugae
+
+    print(f'Maximum length of the source sentnece : {max_len_src}')
+    print(f'Maximum length of the target sentence : {max_len_tgt}')
+
+    # Create the data loaders
+    train_dataloader = Dataloader(train_ds, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = Dataloader(val_ds, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
